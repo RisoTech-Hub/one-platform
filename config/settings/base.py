0b2f7a00d0f55becc939304b0d7c4b9445b20794
thai.ly <thai.ly@masterisehomes.com>
2022-02-16 22:54:26 +0700

    update table ordering

--- config/settings/base.py
+++ config/settings/base.py
@@ -319,6 +319,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
