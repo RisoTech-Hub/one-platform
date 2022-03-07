4adca908045c275cf8b964a9abe9baf4eace3e34
Bin <bin.ngoctu@gmail.com>
2022-03-07 23:27:40 +0700

    fix main logo

--- one/settings/api/serializers.py
+++ one/settings/api/serializers.py
@@ -1,11 +1,15 @@
-from rest_framework.serializers import ModelSerializer, UUIDField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField, UUIDField
 
 from one.settings.models import Setting
 
 
 class SettingSerializer(ModelSerializer):
     id = UUIDField(read_only=True)
+    main_logo = SerializerMethodField()
 
     class Meta:
         model = Setting
         fields = "__all__"
+
+    def get_main_logo(self, obj):
+        return f"<img src='{obj.main_logo.url}'>"
--- one/settings/context_processors.py
+++ one/settings/context_processors.py
@@ -4,4 +4,4 @@ from one.settings.models import Setting
 def setting_settings(request):
     """Expose some application settings in templates."""
     setting = Setting.objects.filter(is_active=True).first()
-    return {"MAIN_LOGO": setting.main_logo}
+    return {"MAIN_LOGO": setting.main_logo.url}
