085c822894cc15fe031410c815274eb6a73f023e
Bin <bin.ngoctu@gmail.com>
2022-03-19 14:29:48 +0700

    update display

--- one/emails/api/serializers.py
+++ one/emails/api/serializers.py
@@ -1,12 +1,22 @@
-from rest_framework.serializers import UUIDField
+from django.utils.translation import gettext_lazy as _
+from rest_framework.serializers import SerializerMethodField, UUIDField
 
-from one.components.serializers import BaseSerializer
+from one.components.serializers import LingualSerializer
 from one.emails.models import EmailTemplate
 
 
-class EmailTemplateSerializer(BaseSerializer):
+class EmailTemplateSerializer(LingualSerializer):
     id = UUIDField(read_only=True)
+    is_protected = SerializerMethodField()
 
     class Meta:
         model = EmailTemplate
         fields = "__all__"
+
+    @staticmethod
+    def get_is_protected(obj):
+        return (
+            f"<span class='badge badge-success'>{_('Protected')}</span>"
+            if obj.is_protected
+            else f"<span class='badge badge-danger'>{_('Not Protect')}</span>"
+        )
--- one/settings/api/serializers.py
+++ one/settings/api/serializers.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework.serializers import SerializerMethodField, UUIDField
 
 from one.components.serializers import BaseSerializer
@@ -7,10 +8,20 @@ from one.settings.models import Setting
 class SettingSerializer(BaseSerializer):
     id = UUIDField(read_only=True)
     main_logo = SerializerMethodField()
+    is_active = SerializerMethodField()
 
     class Meta:
         model = Setting
         fields = "__all__"
 
-    def get_main_logo(self, obj):
+    @staticmethod
+    def get_main_logo(obj):
         return f"<img src='{obj.main_logo.url}'>"
+
+    @staticmethod
+    def get_is_active(obj):
+        return (
+            f"<span class='badge badge-success'>{_('Active')}</span>"
+            if obj.is_active
+            else f"<span class='badge badge-danger'>{_('Not Active')}</span>"
+        )
