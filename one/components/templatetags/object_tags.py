7dd68eb35a11049de2c34fb31c54499035eecacc
Bin <bin.ngoctu@gmail.com>
2022-03-22 00:14:00 +0700

    fix complex

--- one/components/templatetags/object_tags.py
+++ one/components/templatetags/object_tags.py
@@ -12,6 +12,7 @@ def object_value(value, arg):
     # In case get value from dict
     if isinstance(arg, dict) and value in arg:
         return arg[value]
+
     # In case value is tuple for nested object
     if isinstance(value, tuple):
         try:
@@ -19,6 +20,7 @@ def object_value(value, arg):
         except AttributeError:
             data = eval_expression(arg, f"arg.{value[1]}.get_{value[0]}_display()")
         return data
+
     # In case get value in object
     if value not in ["", None] and hasattr(arg, value):
         try:
@@ -28,15 +30,3 @@ def object_value(value, arg):
             data = _(_value) if isinstance(_value, str) else _value
         return data
     return ""
-
-
-@register.filter(name="obj_type")
-def object_type(obj):
-    """Type of object"""
-    return type(obj)
-
-
-@register.filter(name="obj_meta")
-def object_meta(obj):
-    """Meta of object"""
-    return obj.__dict__
