ddc2e78e684b5e7418dfe62f31ff2781e5dbece8
Bin <bin.ngoctu@gmail.com>
2022-04-20 00:40:50 +0700

    Add new packages

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,6 +83,8 @@ THIRD_PARTY_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "drf_spectacular",
+    "webpush",
+    "django_filters",
 ]
 
 LOCAL_APPS = [
@@ -341,6 +343,14 @@ USER_ONLINE_TIMEOUT = 300
 # Number of seconds that we will keep track of inactive users for before
 # their last seen is removed from the cache
 USER_LASTSEEN_TIMEOUT = 60 * 60 * 24 * 7
+
+# Web push notification
+# ------------------------------------------------------------------------------
+WEBPUSH_SETTINGS = {
+    "VAPID_PUBLIC_KEY": env("VAPID_PUBLIC_KEY", default=""),
+    "VAPID_PRIVATE_KEY": env("VAPID_PRIVATE_KEY", default=""),
+    "VAPID_ADMIN_EMAIL": env("VAPID_ADMIN_EMAIL", default=""),
+}
 
 # Your stuff...
 # ------------------------------------------------------------------------------
