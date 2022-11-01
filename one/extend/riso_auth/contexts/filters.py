37e4e962523ef9afea625c4a8df6805fa4db1740
Bin <bin.ngoctu@gmail.com>
2022-11-01 10:22:32 +0700

    filter form

--- one/extend/riso_auth/contexts/filters.py
+++ one/extend/riso_auth/contexts/filters.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 from django_filters import CharFilter, FilterSet
+
+from one.components.widgets import BootstrapInput
 
 
 class GroupFilter(FilterSet):
-    name = CharFilter(lookup_expr="contains")
+    name = CharFilter(
+        field_name="name",
+        lookup_expr="contains",
+        label=_("Name"),
+        widget=BootstrapInput({"placeholder": _("Name")}),
+    )
 
     class Meta:
         model = Group
