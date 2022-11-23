9ef933d7ecc9760131c6047f953e53b5555fd041
Ly Quang Thai <thai.ly@masterisehomes.com>
2022-11-23 22:01:11 +0700

    form drawer

--- one/static/js/widgets/model_select_actions.js
+++ one/static/js/widgets/model_select_actions.js
@@ -31,11 +31,83 @@ function appendModalWithId(selector = 'body', idModal = 'modal_fake', idForm = '
         `)
 }
 
+function appendDrawerWithId(selector = 'body', idDrawer = 'drawer_fake', idForm = '', content = '', target_input = '', close_label = '', save_changes_label = 'Save') {
+    $(selector).append(`
+            <!--begin::Chat drawer-->
+<div id="${idDrawer}" class="bg-body"
+     data-kt-drawer="true"
+     data-kt-drawer-name="user-menu"
+     data-kt-drawer-activate="true"
+     data-kt-drawer-overlay="true"
+     data-kt-drawer-width="{default:'300px', 'md': '500px'}"
+     data-kt-drawer-direction="end"
+     data-kt-drawer-toggle="#kt_drawer_user_menu_toggle"
+     data-kt-drawer-close="#kt_drawer_user_menu_close">
+  <!--begin::Messenger-->
+  <div class="card rounded-0 w-100">
+			<!--begin::Card header-->
+			<div class="card-header pe-5">
+				<!--begin::Title-->
+				<div class="card-title">
+					<!--begin::User-->
+					<div class="d-flex justify-content-center me-3">
+						<div class="me-4">
+							<img src="" alt="img_thumbnail" class="img-fluid" id="img_thumbnail_file" onerror="this.src='/assets/media/icons/duotune/general/gen006.svg'">
+						</div>
+						<div class="align-self-center">
+							<a href="javascript:void(0);" class="fs-4 fw-bold text-gray-900 text-hover-primary me-1 lh-1" id="span_detail_file_name"></a>
+						</div>
+					</div>
+					<!--end::User-->
+				</div>
+				<!--end::Title-->
+				<!--begin::Card toolbar-->
+				<div class="card-toolbar">
+					<!--begin::Close-->
+					<div class="btn btn-sm btn-icon btn-active-light-primary" id="kt_drawer_example_dismiss_close" data-kt-drawer-dismiss="true">
+						<!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+						<span class="svg-icon svg-icon-2">
+															<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+																<rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="currentColor"></rect>
+																<rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="currentColor"></rect>
+															</svg>
+														</span>
+						<!--end::Svg Icon-->
+					</div>
+					<!--end::Close-->
+				</div>
+				<!--end::Card toolbar-->
+			</div>
+
+			<!--end::Card header-->
+			<!--begin::Card body-->
+			<div class="card-body pt-0 hover-scroll-overlay-y">
+				<form id="${idForm}">
+				    ${content}
+                </form>
+			</div>
+			<!--end::Card body-->
+			<!--begin::Card footer-->
+			<div class="card-footer text-center">
+				<!--begin::Dismiss button-->
+			    <button class="btn btn-light-danger me-2" data-kt-drawer-dismiss="true">Close</button>
+			    <!--end::Dismiss button-->
+			    <button type="submit" form="${idForm}" class="btn btn-primary">${save_changes_label}</button>
+			</div>
+			<!--end::Card footer-->
+		</div>
+  <!--end::Messenger-->
+</div>
+<!--end::Chat drawer-->
+        `)
+}
+
 $(document).ready(function () {
     $(document).on('click', '[quick-create-button], [quick-update-button], [quick-add-button]', function () {
         const action_url = $(this).data('action-url')
         const target_input = $(this).data('target-input')
         console.log('action_url', action_url)
+        const self = this;
 
         if (action_url.indexOf('popup') === -1) {
             window.location.href = action_url;
@@ -43,18 +115,34 @@ $(document).ready(function () {
         }
 
         $.ajax({
-            url: action_url, method: "get", data: null, success: function (response) {
+            url: action_url, method: "get", data: null,
+            success: function (response) {
                 // get form html return success
                 // console.log('-----', response)
                 const timestamp = Date.now();
-                const idModal = 'modal_fake_' + timestamp;
-                const idForm = 'form_fake_' + timestamp;
-                appendModalWithId('body', idModal, idForm, response, target_input, $('#id_btn_close_label').val(), $('#id_btn_save_changes_label').val(),)
-                $('#id_modal_title').text($('#id_form_title').val())
-                $('#' + idModal).modal('show')
-                $('#' + idModal).find('form').attr('id', idForm)
+                const theme = $(self).data('theme');
+                console.log('theme', theme)
+                if (theme === 'drawer') {
 
-                reInitAllWidget()
+                    var idDrawer = "kt_drawer_fake_" + timestamp
+                    var idDrawerForm = "kt_form_drawer_fake_" + timestamp
+
+                    appendDrawerWithId('body', idDrawer, idDrawerForm, response, target_input, $('#id_btn_close_label').val(), $('#id_btn_save_changes_label').val(),)
+
+                    KTDrawer.createInstances();
+                    var drawerElement = document.querySelector('#' + idDrawer);
+                    var drawer = KTDrawer.getInstance(drawerElement);
+                    drawer.show();
+                } else {
+                    const idModal = 'modal_fake_' + timestamp;
+                    const idForm = 'form_fake_' + timestamp;
+                    appendModalWithId('body', idModal, idForm, response, target_input, $('#id_btn_close_label').val(), $('#id_btn_save_changes_label').val(),)
+                    $('#id_modal_title').text($('#id_form_title').val())
+                    $('#' + idModal).modal('show')
+                    $('#' + idModal).find('form').attr('id', idForm)
+
+                    reInitAllWidget();
+                }
 
             }, error: function (request, status, error) {
                 toastr.error(`${JSON.stringify(request)}\n${status}\n${error}`)
