917bdc132697bcc8d27b3dfb0c8fc7b18de14641
Ly Quang Thai <thai.ly@masterisehomes.com>
2022-11-24 23:06:02 +0700

    update

--- one/static/js/widgets/table_multiple_formset.js
+++ one/static/js/widgets/table_multiple_formset.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
 
+    $(document).on('click', '[data-kt-element="remove-item"]', function (e) {
+
+    })
+
+    $(document).on('click', '[kt-add-line-formset="true"]', function (e) {
+        e.preventDefault();
+        const $targetAppend = $($(this).attr('kt-target-append'));
+        const $targetTotal = $($(this).attr('kt-target-total'));
+        let counter = parseInt($targetTotal.val());
+        $targetTotal.val(counter + 1);
+        const cloneElement = $($(this).attr('kt-target-element')).html().replace(/__index__/g, counter++);
+        const html = $.parseHTML(cloneElement)
+        console.log('cloneElement', cloneElement, html);
+
+        $targetAppend.append(html);
+    })
 })
