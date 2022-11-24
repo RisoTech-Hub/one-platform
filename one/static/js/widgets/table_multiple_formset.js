d54c56bdb526f1dc2e026538e62659b8298c4087
Ly Quang Thai <thai.ly@masterisehomes.com>
2022-11-24 23:53:03 +0700

    update

--- one/static/js/widgets/table_multiple_formset.js
+++ one/static/js/widgets/table_multiple_formset.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
 
     $(document).on('click', '[data-kt-element="remove-item"]', function (e) {
+        const targetElement = $(this).attr('kt-target-element');
+        $(targetElement).prop('checked', true).trigger('change');
 
+        const $parent = $(this).parents('[data-kt-element="items"]');
+
+        $parent.addClass('d-none')
     })
 
     $(document).on('click', '[kt-add-line-formset="true"]', function (e) {
@@ -9,8 +14,8 @@ $(document).ready(function () {
         const $targetAppend = $($(this).attr('kt-target-append'));
         const $targetTotal = $($(this).attr('kt-target-total'));
         let counter = parseInt($targetTotal.val());
+        const cloneElement = $($(this).attr('kt-target-element')).html().replace(/__prefix__/g, counter);
         $targetTotal.val(counter + 1);
-        const cloneElement = $($(this).attr('kt-target-element')).html().replace(/__index__/g, counter++);
         const html = $.parseHTML(cloneElement)
         console.log('cloneElement', cloneElement, html);
 
