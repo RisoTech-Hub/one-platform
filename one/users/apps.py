c5256454fe202f129c6b0e5a4bdb76e02e487807
Bin <bin.ngoctu@gmail.com>
2022-11-25 22:00:00 +0700

    Update users

--- one/users/apps.py
+++ one/users/apps.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class UsersConfig(AppConfig):
     name = "one.users"
-    verbose_name = _("Users")
+    verbose_name = _("User")
 
     def ready(self):
         try:
