76ef6021778ed9b0b7b5e53c90e75d1fe656c9a2
Bin <bin.ngoctu@gmail.com>
2022-11-25 12:54:25 +0700

    config

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,10 +72,10 @@ DJANGO_APPS = [
     "django.forms",
 ]
 EXTENDED_APPS = [
+    "one.contrib.auth.contexts",
     "one.contrib.contenttypes.configs",
     "one.contrib.sites.settings",
     "one.extend.riso_allauth",
-    "one.contrib.auth.contexts",
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
@@ -301,6 +301,8 @@ if USE_TZ:
 CELERY_BROKER_URL = env("CELERY_BROKER_URL")
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_backend
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-extended
+CELERY_RESULT_EXTENDED = True
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-accept_content
 CELERY_ACCEPT_CONTENT = ["json"]
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-task_serializer
@@ -315,6 +317,10 @@ CELERY_TASK_TIME_LIMIT = 5 * 60
 CELERY_TASK_SOFT_TIME_LIMIT = 60
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#beat-scheduler
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker-send-task-events
+CELERY_WORKER_SEND_TASK_EVENTS = True
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-task_send_sent_event
+CELERY_TASK_SEND_SENT_EVENT = True
 # django-allauth
 # ------------------------------------------------------------------------------
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
