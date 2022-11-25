88e2ae8b0630e8fad92e7b95d90f45334df41a96
Ly Quang Thai <thai.ly@masterisehomes.com>
2022-11-25 18:50:02 +0700

    update

--- one/static/js/utilities.js
+++ one/static/js/utilities.js
@@ -5,19 +5,25 @@ const debounce = (func, delay) => {
         const context = this
         const args = arguments
         clearTimeout(debounceTimer)
-        debounceTimer
-            = setTimeout(() => func.apply(context, args), delay)
+        debounceTimer = setTimeout(() => func.apply(context, args), delay)
     }
 }
 
 
 function parseErrorAjax(responseJSON) {
     $.each(responseJSON, function (key, value) {
-        $.each(value, function (index, _value) {
+        $.each(value, function (_key, _value) {
             if (key === "__all__") {
                 toastr.error(_value, "{% translate 'Error' %}");
             } else {
-                toastr.error(_value.replace(/field/g, key));
+                console.log('_value', _value);
+                if (Array.isArray(_value)) {
+                    _value.map(function (item) {
+                        toastr.error(item.replace(/field/g, _key));
+                    })
+                } else {
+                    toastr.error(_value.replace(/field/g, _key));
+                }
             }
         });
     });
